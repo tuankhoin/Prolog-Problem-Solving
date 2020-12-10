a4a398f60c5b8d2f02da0b884d8a5904635fd4d5
tuankhoin <tuankhoin@student.unimelb.edu.au>
2020-12-10 16:33:44 +1100

    Update sudoku.pl

--- sudoku.pl
+++ sudoku.pl
@@ -2,8 +2,7 @@ alldiff(L) :-
     include(nonvar,L,Numbers),
     exclude(nonvar,L,Vars),
     length(L, Length),
-    Max is Length - 1,
-    findall(X, between(0,Max,X), Xs),
+    findall(X, between(1,Length,X), Xs),
     subtract(Xs, Numbers, Available),
     permutation(Available, Vars).
     
