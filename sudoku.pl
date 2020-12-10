6fe77ab85058feb65eebd5914bfc45498a4a81a2
tuankhoin <tuankhoin@student.unimelb.edu.au>
2020-12-10 22:37:31 +1100

    Update sudoku.pl

--- sudoku.pl
+++ sudoku.pl
@@ -12,3 +12,21 @@ sudoku(L) :-
     maplist(alldiff, L),
     transpose(L, LTranspose),
     maplist(alldiff, LTranspose). 
+
+run_sudoku(Problem) :-
+    sudoku(Problem),
+    maplist(portray_clause, Problem).
+
+% Try run_sudoku([[1,2,3],[2,X1,1],[X2,X3,X4]]).
+
+/* Use this if it makes you feel easier to write the input problem:
+
+run_sudoku([
+
+[1 , 2 , 3 ],
+[2 , X1, 1 ],
+[X2, X3, X4]
+        
+]).
+
+*/
